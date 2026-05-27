5fceb5e5a960376fcaec20bd30605434fbb61ed2
hayoung1109 <hadoll201109@gamil.com>
2026-05-27 17:22:17 +0900

    스토어 헤더

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,22 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-// import ThemeProvider from "@/components/ThemeProvider";
-import QuickMenu from "@/components/QuickMenu";
 
 export const metadata: Metadata = {
   title: "LAFTEL",
   description: "라프텔 애니메이션 OTT",
-  icons: {
-    icon: '/favicon.png',
-  },
-}
+  icons: { icon: '/favicon.png' },
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ko" suppressHydrationWarning>
       <body className="min-h-full flex flex-col transition-colors">
-        {/* <ThemeProvider> */}
-        <Header />
-        <main>{children}</main>
-        <Footer />
-        {/* <QuickMenu /> */}
-        {/* </ThemeProvider> */}
+        {children}
       </body>
     </html>
   );
